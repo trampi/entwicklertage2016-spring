a60760a2a074c795ba8ff0082df01f3e5b210e5d
Fabian Trampusch <fabian.trampusch@freenet.de>
2016-02-09 15:05:29 +0100

    Adds the complete magic of Spring Data Rest

--- src/main/java/org/gradle/Application.java
+++ src/main/java/org/gradle/Application.java
@@ -2,9 +2,11 @@ package org.gradle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 
 @SpringBootApplication
-public class Application {
+public class Application extends RepositoryRestMvcConfiguration {
 	
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
